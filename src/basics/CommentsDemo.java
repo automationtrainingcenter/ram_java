474753ab8d6f81490dc54f95ad44624da122c4be
automationtrainingcenter <atcsurya@gmail.com>
2021-02-05 09:02:00 +0530

    comments and variable intro

---
+++ src/basics/CommentsDemo.java
@@ -0,0 +1,20 @@
+package basics;
+/**
+ * This class explains comments in Java. Comments are nothing but 
+ * statements which will not run or execute.
+ */
+
+public class CommentsDemo {
+	
+	// single line comment
+	/*
+	 * this is multiline
+	 * comment
+	 */
+	
+	/**
+	 * this is documentation
+	 * comment
+	 */
+
+}
